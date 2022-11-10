6a528cb70d21e3fb3bad1b80a4540d5ac890c425
Darth-Malak <111587763+Darth-Malak@users.noreply.github.com>
2022-11-10 09:11:41 -0700

    challenge not completed

--- Challenge2.6/Assets/Sunnyland/artwork/Sprites/player/idle/NewBehaviourScript.cs
+++ Challenge2.6/Assets/Sunnyland/artwork/Sprites/player/idle/NewBehaviourScript.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    private player sphere;
+    private Vector3 scaleChange, positionChange;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Camera.main.clearFlags = CameraClearFlags.SolidColor;
+        // Create a sphere at the origin.
+        sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.position = new Vector3(0, 0, 0);
+
+        // Create a plane and move down by 0.5.
+        GameObject plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        plane.transform.position = new Vector3(0, -0.5f, 0);
+
+        // Change the floor color to blue.
+        // The blue material is present in Resources and not created in this script.
+        Renderer rend = plane.GetComponent<Renderer>();
+        rend.material = Resources.Load<Material>("blue");
+
+        scaleChange = new Vector3(-0.01f, -0.01f, -0.01f);
+        positionChange = new Vector3(0.0f, -0.005f, 0.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        sphere.transform.localScale += scaleChange;
+        sphere.transform.position += positionChange;
+
+        // Move upwards when the sphere hits the floor or downwards
+        // when the sphere scale extends 1.0f.
+        if (sphere.transform.localScale.y < 0.1f || sphere.transform.localScale.y > 1.0f)
+        {
+            scaleChange = -scaleChange;
+            positionChange = -positionChange;
+        }
     }
 }
